e31059c7ca062afc44e0fa9052a0d9159fbd573e
akiosmonov <darken452@gmail.com>
2026-02-11 21:21:51 +0600

    versel

--- src/components/common/ui/Button.tsx
+++ src/components/common/ui/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from './Button.module.scss';
+import s from './button.module.scss';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'outline' | 'yellow';
