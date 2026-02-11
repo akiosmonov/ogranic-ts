1fd2c81aae67281101c8307669d7af152a648a49
akiosmonov <darken452@gmail.com>
2026-02-11 21:19:28 +0600

    versel

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path"; // Нужно установить: npm install -D @types/node
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
