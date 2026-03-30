7dd8b7671dd72a4647a0b893d932f802dd2e031a
akiosmonov <darken452@gmail.com>
2026-03-30 18:31:23 +0600

    fix: fix the files and the vite conf

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-import path from "path"; // Нужно установить: npm install -D @types/node
+import path from "path"; 
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
