75bfa2063ab8c79c1bc8e77843ebfd520d466e02
akiosmonov <darken452@gmail.com>
2026-03-30 18:49:38 +0600

    fix: final build configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"; // Нужно установить: npm install -D @types/node
+import path from "path";
+import { fileURLToPath } from "url";
+var __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     resolve: {
